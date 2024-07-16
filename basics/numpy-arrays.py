ca7b8d79fec8552eedda272b30f2d2a1a1d1394a
emanuelefavero <emafaux89@gmail.com>
2024-07-17 00:09:00 +0200

    add basics/numpy-arrays - masking

--- basics/numpy-arrays.py
+++ basics/numpy-arrays.py
@@ -13,8 +13,27 @@ print(arr[0, 1])  # 7.25, first row, second column
 
 # Select a single row
 print("Single row:")
-print(arr[0])  # [3. 7.25 22.]
+print(arr[0])  # [3. 7.25 22]
 
 # Select a single column
 print("Single column:")
 print(arr[:, 1])  # [ 7.25 71.2833  7.925 53.1 ...]
+
+# MASKING
+# Create a mask to filter values
+# TIP: A mask is a boolean array that can be used to filter values
+
+# Select all rows where Age is less than 30
+mask = arr[:, 2] < 30
+print("Mask:")
+print(mask)  # [ True False  True ...]
+print("Filtered values:")
+print(arr[mask])  # Show only rows where Age is less than 30
+# TIP: We can also use the mask directly in the array `arr[arr[:, 2] < 30]`
+
+# Select all rows where Pclass is equal to 1, show only the Age column
+arr = df[["Pclass", "Age"]].values
+first_class_passengers = arr[arr[:, 0] == 1]
+first_class_passengers_ages = first_class_passengers[:, 1]
+print("First class passengers ages:")
+print(first_class_passengers_ages)  # [38 35 ...]
