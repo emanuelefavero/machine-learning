c4f22b5e29a7d497c0925a166adbcd3716a50169
emanuelefavero <emafaux89@gmail.com>
2024-07-16 19:18:59 +0200

    add basics/numpy-basics values attribute

---
+++ basics/numpy-basics.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv("data/titanic.csv")
+
+# TIP: You can use the .values attribute to get the values as a NumPy array
+
+# Get values from a single column
+print("Single column:")
+print(df[["Fare"]].values)
+
+# Get values from multiple columns
+print("Multiple columns:")
+print(df[["Fare", "Age"]].values)
+
+# Get all values from the DataFrame
+print("All values:")
+print(df.values)
