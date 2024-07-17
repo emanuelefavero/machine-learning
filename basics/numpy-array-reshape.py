42d88e33836fb62f407d04f011efab1a86adb132
emanuelefavero <emafaux89@gmail.com>
2024-07-17 17:53:34 +0200

    add basics/numpy-array-reshape

---
+++ basics/numpy-array-reshape.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# * RESHAPE ARRAY
+# The reshape() method changes the shape of the array
+
+# Reshape 1-D array to 2-D array
+array = np.array([1, 2, 3, 4])
+reshaped = array.reshape(2, 2)
+print(reshaped)  # [[1 2] [3 4]]
+
+# Reshape 1-D array to 3-D array
+array = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+reshaped = array.reshape(2, 2, 2)
+print(reshaped)  # [[[1 2] [3 4]] [[5 6] [7 8]]]
+
+# TIP: As long as the number of elements remains the same,
+# you can reshape the array
+
+# UNKNOWN DIMENSION
+# You can pass -1 to the reshape method to let NumPy calculate
+# the unknown dimension
+
+# Reshape 2-D array to 1-D array
+array = np.array([[1, 2], [3, 4]])
+# reshaped = array.reshape(4)  # 4 elements
+reshaped = array.reshape(-1)  # -1 means unknown dimension
+print(reshaped)  # [1 2 3 4]
