6869d8c63c3b7c3f3013df4e3d94664e18f1e0e3
emanuelefavero <emafaux89@gmail.com>
2024-07-18 23:41:28 +0200

    add basics/plotting-basics - add line, legend, colors

--- basics/plotting-basics.py
+++ basics/plotting-basics.py
@@ -7,10 +7,10 @@ import pandas as pd
 
 df = pd.read_csv("data/titanic.csv")
 
-# Create a scatter plot
-plt.scatter(df["Age"], df["Fare"])
+# Create a scatter plot and choose the colors of the dots
+plt.scatter(df["Age"], df["Fare"], c="#f0f")
 
-# Create a scatter plot with colored dots
+# Create a scatter plot with colored dots based on the "Pclass" column
 # TIP: The "Pclass" column has 3 unique values (1, 2, 3),
 # those will be represented by different colors
 # plt.scatter(df["Age"], df["Fare"], c=df["Pclass"])  # ! UNCOMMENT TO SEE
@@ -21,6 +21,13 @@ plt.ylabel("Fare")
 
 # Add a title
 plt.title("Titanic passengers")
+
+# Add a line
+# first two values are the x-axis, the last two are the y-axis
+plt.plot([22, 38], [7, 71], color="#78ff78bf")
+
+# Add a legend
+plt.legend(["Passengers", "Line"])
 
 
 # Show the plot
